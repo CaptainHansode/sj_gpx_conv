3144117126dadacb758ba218ebc8c519b9e5d00f
CaptainHansode <sakaiden@live.jp>
2025-03-14 10:08:04 +0900

    feat:pyside2-uic.exeを検索取得

--- ui2py.bat
+++ ui2py.bat
@@ -4,16 +4,40 @@ echo.
 
 @REM Py ver
 set pyverdir=Python310
+
+
+
 set pyside2uicpath=%LOCALAPPDATA%\Programs\Python\%pyverdir%\Scripts
 
 @REM UI File
 set in_file=%~dp0sj_gpx_conv.ui
 set out_file=%~dp0sj_gpx_conv_ui.py
 
-echo.
-echo %pyside2uicpath%\pyside2-uic.exe -o %out_file% %in_file%
-%pyside2uicpath%\pyside2-uic.exe -o %out_file% %in_file%
-echo.
+set "target_dir=%UserProfile%\AppData\Local\Programs\Python"
+set "filename=pyside2-uic.exe"
+set "PYSIDE2_PATH="
+for /r "%target_dir%" %%f in (%filename%) do (
+    @REM echo %%f | findstr /i /c:"__pycache__" >nul
+    if Exist %%f (
+        set "PYSIDE2_PATH=%%f" goto :found
+    )
+)
+
+:found
+if defined PYSIDE2_PATH (
+    echo ================
+    echo Found pyside2-uic.exe file: %PYSIDE2_PATH%
+    echo ================
+    echo Compile start
+    echo %PYSIDE2_PATH% -o %out_file% %in_file%
+    %PYSIDE2_PATH% -o %out_file% %in_file%
+    echo ================
+
+) else (
+    echo File not found.
+)
+
 echo.
 
 pause
+Rem cmd.exe /K
